93d0ae2755065b487def7897ad7cc24c57b61619
chenjiaxing <jiaxing7676@163.com>
2022-06-25 21:15:33 +0800

    完成鉴权，权限信息写在程序里

--- src/main/java/cn/ishangit/security/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/cn/ishangit/security/service/impl/UserDetailsServiceImpl.java
@@ -10,6 +10,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -29,9 +32,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         if(Objects.isNull(user)){
             throw new RuntimeException("用户名或密码错误");
         }
-        //TODO 根据用户查询权限信息 添加到LoginUser中
-
+        List<String> list = new ArrayList<>(Arrays.asList("test"));
         //封装成UserDetails对象返回
-        return new LoginUser(user);
+        return new LoginUser(user,list);
     }
 }
